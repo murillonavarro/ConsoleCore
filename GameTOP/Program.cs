9e28a08bc5da58ac7807abb2826ce5145abd9406
murillonavarro <42552033+murillonavarro@users.noreply.github.com>
2021-06-08 15:27:20 -0300

    first commit

---
+++ GameTOP/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA(new Jogador("Ronaldo"));
+            jogo.IniciarJogo();
+        }
+    }
+
+    class Jogador
+    {
+        public readonly string _Nome;
+
+        public Jogador(string nome)
+        {
+            _Nome = nome;
+        }
+        public void chuta()
+        {
+            Console.Write($"{_Nome} está chutando");
+        }
+
+        public void Corre()
+        {
+            Console.Write($"{_Nome} está correndo");
+        }
+
+        public void passe()
+        {
+            Console.Write($"{_Nome} está passando");
+        }
+    }
+}
